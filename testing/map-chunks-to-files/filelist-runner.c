ce9c60d9d3066257bea7b71d3cb847cb3892c58d
runefriborg <rune.m.friborg@gmail.com>
2015-05-07 15:47:00 +0200

    cleaning

--- testing/map-chunks-to-files/filelist-runner.c
+++ testing/map-chunks-to-files/filelist-runner.c
@@ -3,8 +3,10 @@
 #include <sys/types.h>
 #include <string.h>
 #include <errno.h>
+
 /* "readdir" etc. are defined here. */
 #include <dirent.h>
+
 /* limits.h defines "PATH_MAX". */
 #include <limits.h>
 
@@ -13,20 +15,27 @@
 
 #include "mutexqueue.h"
 
-struct circqueue * dirqueue;
+// Global values shared between threads
+struct mutexqueue * dir_queue;
 int SKIPCHARS;
 
-/* List the files in "dir_name". */
-void * list_dir (void * x) {
+
+/* Worker thread for listing dirs */
+void * dir_worker (void * fileoutput) {
+
   DIR * d;
   char * dir_name;
-  FILE * fp = x;
+  FILE * fp = fileoutput;
   struct dirent dirent_data; 
  
-  while (1) {        
-    dir_name = dequeue(dirqueue);
+
+  while (1) {
+
+    /* Blocking dequeue */
+    dir_name = dequeue(dir_queue);
 
     if (dir_name == NULL) {
+      /* The mutexqueue is empty and all threads have completed */
       break;
     }
 
@@ -39,6 +48,7 @@ void * list_dir (void * x) {
                  dir_name, strerror (errno));
         exit (EXIT_FAILURE);
     }
+
     while (1) {
         struct dirent * entry;
         const char * d_name;
@@ -57,6 +67,7 @@ void * list_dir (void * x) {
 	  break;
 	}
 
+	/* Write the file entry to output file */
         d_name = entry->d_name;
         if (! (entry->d_type & DT_DIR)) {
           if (strlen(dir_name) > SKIPCHARS) {
@@ -69,7 +80,6 @@ void * list_dir (void * x) {
         if (entry->d_type & DT_DIR) {
 
             /* Check that the directory is not "d" or d's parent. */
-            
             if (strcmp (d_name, "..") != 0 &&
                 strcmp (d_name, ".") != 0) {
                 int path_length;
@@ -82,8 +92,8 @@ void * list_dir (void * x) {
                     exit (EXIT_FAILURE);
                 }
 
-		/* add path to dirqueue */
-		if (enqueue(dirqueue, path) != 0) {
+		/* add path to dir_queue */
+		if (enqueue(dir_queue, path) != 0) {
                     fprintf (stderr, "Enqueue failed\n");
 		    exit (EXIT_FAILURE);
                 }
@@ -97,6 +107,7 @@ void * list_dir (void * x) {
         exit (EXIT_FAILURE);
     }
     
+    /* Free dequeued element from mutexqueue */
     free(dir_name);
   }
   pthread_exit(NULL);
@@ -108,9 +119,11 @@ int main(int argc, char *argv[]) {
   FILE *fp[READDIR_THREADS];
   int i;
   char tmp[64];
-  
+
+  /* Use malloc for root element! */
   char * dir = (char *) malloc(PATH_MAX*sizeof(char));
  
+  /* Get args */
   if(argc == 3) {
     snprintf (dir, PATH_MAX, "%s", argv[2]);
     dir= realpath(dir, NULL);
@@ -120,21 +133,21 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  dirqueue= mutexqueue(READDIR_THREADS, QUEUE_SIZE);
-  enqueue(dirqueue, dir);
+  /* Allocate mutexqueue and enqueue root element */
+  dir_queue= mutexqueue_create(READDIR_THREADS, QUEUE_SIZE);
+  enqueue(dir_queue, dir);
 
   /* Create output files */
   for (i = 0; i<READDIR_THREADS; i++) {
     sprintf(tmp, "output.%d", i);
     fp[i] = fopen(tmp, "w");
   }
-     
 
   /* For portability, explicitly create threads in a joinable state */
   pthread_attr_init(&attr);
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
   for(i = 0; i < READDIR_THREADS; i++) {
-    pthread_create(&threads[i], &attr, list_dir, fp[i]);
+    pthread_create(&threads[i], &attr, dir_worker, fp[i]);
   }
 
   /* Wait for all threads to complete */
@@ -149,7 +162,7 @@ int main(int argc, char *argv[]) {
 
   /* Clean up and exit */
   pthread_attr_destroy(&attr);
-  mutexqueue_destroy(dirqueue);
+  mutexqueue_destroy(dir_queue);
   pthread_exit(NULL);
 
   return 0;
--- testing/map-chunks-to-files/mutexqueue.h
+++ testing/map-chunks-to-files/mutexqueue.h
@@ -1,7 +1,35 @@
 #include <pthread.h>
+/*
+ * A queue implemented in a circular array.
+ * Thread-safe functions:
+ *   enqueue(...)
+ *   dequeue(...)
+ * 
+ * dequeue blocks when the queue is empty.
+ * As a shutdown feature, all threads are released from blocking, if all threads are waiting on dequeue()
+ *
+ * This queue is particularly well suited for converting recursive methods into a multithreading method.
+ * Example:
+ * compute() {
+ *   while (1) {
+ *     next = dequeue();
+ *     if (next == NULL)
+ *       break;
+ *     if (..)
+ *       enqueue(..);
+ *     if (..)
+ *       enqueue(..);
+ * }
+ * main() {
+ *   queue_create(..)
+ *   enqueue(root_problem)
+ *   start compute threads
+ *   join threads
+ *   queue_destroy(..)
+ * }
+ */
 
-
-struct circqueue {
+struct mutexqueue {
   pthread_mutex_t _mutex;
   pthread_cond_t _cond;
   int front,rear;
@@ -12,8 +40,8 @@ struct circqueue {
 };
 
 
-struct circqueue *mutexqueue(int thread_count, int size) {
-   struct circqueue *q=malloc(sizeof(struct circqueue));
+struct mutexqueue *mutexqueue_create(int thread_count, int size) {
+   struct mutexqueue *q=malloc(sizeof(struct mutexqueue));
    if(!q)return NULL;
    q->thread_count=thread_count;
    q->thread_waiting=0;
@@ -30,27 +58,27 @@ struct circqueue *mutexqueue(int thread_count, int size) {
    return q;
 }
 
-void mutexqueue_destroy(struct circqueue *q) {
+void mutexqueue_destroy(struct mutexqueue *q) {
   pthread_mutex_destroy(&q->_mutex);
   pthread_cond_destroy(&q->_cond);
   free(q->array);
   free(q);
 }
 
-int isemptyqueue(struct circqueue *q) {
+int isemptyqueue(struct mutexqueue *q) {
    return(q->front==-1);
 }
 
-int isfullqueue(struct circqueue *q) {
+int isfullqueue(struct mutexqueue *q) {
    return((q->rear+1)%q->capacity==q->front);
 }
 
-int queuesize(struct circqueue *q) {
+int queuesize(struct mutexqueue *q) {
    return(q->capacity-q->rear+q->front+1)%q->capacity;
 }
 
 
-int enqueue(struct circqueue *q,char * x) {
+int enqueue(struct mutexqueue *q,char * x) {
 
   pthread_mutex_lock(&q->_mutex);
   if (isfullqueue(q)) {
@@ -71,7 +99,7 @@ int enqueue(struct circqueue *q,char * x) {
   return 0;
 }
 
-char * dequeue(struct circqueue *q) {
+char * dequeue(struct mutexqueue *q) {
    char * data;
 
    pthread_mutex_lock(&q->_mutex);
