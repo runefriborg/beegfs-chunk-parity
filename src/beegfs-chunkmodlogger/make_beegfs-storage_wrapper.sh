7a28d368c07a12aef849ae6b9f6b3d45cadb89d3
Anders Egerup Halager <aeh@birc.au.dk>
2015-09-08 10:38:50 +0200

    Slightly improved the install procedure
    The wrapper can't be installed on a management node, so we check that
    the beegfs-storage script (or our wrapper) is present before installing
    it.
    Also so minor changes to the Makefile, so that `make install` works
    without calling make on its own first.

--- src/beegfs-chunkmodlogger/make_beegfs-storage_wrapper.sh
+++ src/beegfs-chunkmodlogger/make_beegfs-storage_wrapper.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f /etc/init.d/beegfs-storage ]; then
+    echo "Skipping beegfs-storage wrapper"
+    exit 0
+fi
+
 # Get the binary from the init.d-script
 SERVICE_NAME=$(grep SERVICE_NAME= /etc/init.d/beegfs-storage | cut -d= -f 2)
 APP_BIN=$(grep APP_BIN=      /etc/init.d/beegfs-storage | cut -d= -f 2)
