26de9ff4eaffef854c0ff9faf81183ad2b1cb21e
Anders Egerup Halager <aeh@birc.au.dk>
2015-09-18 12:25:47 +0000

    Minor changes

--- src/bp-changelogger/changelogger.c
+++ src/bp-changelogger/changelogger.c
@@ -14,22 +14,22 @@
 #include <limits.h>
 
 
-#define MAX_OPEN_FILES 65535
-#define MAX_PATH_LENGTH 512
-#define CHANGELOGROTATE 86400
-#define CHANGELOGFOLDER      "/dev/shm/beegfs-changelog/"
-#define LOG                  "/var/log/beegfs-changelog/intercept.log"
-#define ERRORLOG             "/var/log/beegfs-changelog/error.log"
-#define LOGDIR               "/var/log/beegfs-changelog"
-
+#define MAX_OPEN_FILES      16384
+#define MAX_PATH_LENGTH     512
+#define CHANGELOGROTATE     86400
+#define CHANGELOGFOLDER     "/dev/shm/beegfs-changelog/"
+#define LOG                 "/var/log/beegfs-changelog/intercept.log"
+#define ERRORLOG            "/var/log/beegfs-changelog/error.log"
+#define LOGDIR              "/var/log/beegfs-changelog"
 
 #define DEBUG true
 
+#define SAFE_TO_IGNORE ((char *)-1)
 
-/* Operations for the `flock' call.  */
-#define	LOCK_SH	1	/* Shared lock.  */
-#define	LOCK_EX	2 	/* Exclusive lock.  */
-#define	LOCK_UN	8	/* Unlock.  */
+/* Operations for the `flock' call. */
+#define    LOCK_SH    1    /* Shared lock.    */
+#define    LOCK_EX    2    /* Exclusive lock. */
+#define    LOCK_UN    8    /* Unlock.         */
 
 /* Apply or remove an advisory lock, according to OPERATION,
    on the file FD refers to.  */
@@ -38,25 +38,18 @@ extern int flock (int __fd, int __operation);
 
 char *open_files[MAX_OPEN_FILES];
 char pathbuf[MAX_OPEN_FILES*MAX_PATH_LENGTH];
-char *dirpath = NULL;
-char *storage_id = NULL;
+int is_initialized = 0;
+char dirpath[PATH_MAX] = {0};
+char storage_id[PATH_MAX] = {0};
 __thread FILE *log_fd = NULL;
 __thread time_t log_create_t = 0;
-__thread char *rand_log_name = NULL;
+__thread char *log_name = NULL;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 /* function pointers to the original functions */
 static int (*_original_openat)(int dirfd, const char *pathname, int flags, mode_t mode);
 static int (*_original_unlinkat)(int dirfd, const char *pathname, int flags);
 static int (*_original_close)(int fd);
-
-inline void initlog() {
-  int ret = mkdir(LOGDIR, S_IRUSR | S_IWUSR | S_IXUSR);
-  if(ret != 0 && errno != EEXIST) {
-    fprintf(stderr, "Could not create logdir '%s'.\n", LOGDIR);
-    return;
-  }
-}
 
 static void write_log_msg(const char *basename, const char *msg) {
   FILE *f;
@@ -74,6 +67,7 @@ static void write_log_msg(const char *basename, const char *msg) {
     struct timeval tv;
     gettimeofday(&tv, NULL);
     fprintf(f, "%lu.%lu %s", (unsigned long)tv.tv_sec, (unsigned long)tv.tv_usec, msg);
+    fflush(f);
     fclose(f);
   }
   pthread_mutex_unlock(&lock);
@@ -98,7 +92,7 @@ static void writelog(const char *format,...) {
 }
 
 
-inline void debuglog(const char *format,...) {
+static void debuglog(const char *format,...) {
 #ifdef DEBUG
   char msg_a[200] = {0};
   char msg[200] = {0};
@@ -114,7 +108,8 @@ inline void debuglog(const char *format,...) {
 static
 void write_changelog(const char *format,...) {
   /* 1. Check whether or not we should start a new file. */
-  if((time(NULL)-CHANGELOGROTATE) > log_create_t) {
+  time_t now = time(NULL);
+  if((now-CHANGELOGROTATE) > log_create_t) {
     if(log_fd != NULL) {
       flock(fileno(log_fd),LOCK_UN);
       fclose(log_fd);
@@ -124,17 +119,17 @@ void write_changelog(const char *format,...) {
 
   /* 2. Make sure we have an open file to write to */
   if(log_fd == NULL) {
-    log_create_t = time(NULL);
-    asprintf(&rand_log_name,
-        "%s/%s-%ld-threadid=%08x",
+    log_create_t = now;
+    asprintf(&log_name,
+        "%s/%s-%ld-%08x",
         CHANGELOGFOLDER,
         storage_id,
         log_create_t,
         (unsigned int)pthread_self());
-    log_fd = fopen(rand_log_name,"a");
+    log_fd = fopen(log_name,"a");
 
     if(log_fd == NULL) {
-      errorlog("Couldn't create file %s.",rand_log_name);
+      errorlog("Couldn't create file %s.\n",log_name);
       return;
     }
 
@@ -151,54 +146,57 @@ void write_changelog(const char *format,...) {
 }
 
 static void init_once(int dirfd) {
-  /* On the first openat64 intercepted, lookup the path of dirfd on store
-   * it.
-   */
-  if(dirpath == NULL) {
+  /* On the first openat64 intercepted, store the path of dirfd */
+  if(is_initialized == 0) {
     pthread_mutex_lock(&lock);
+    if (is_initialized != 0) {
+      pthread_mutex_unlock(&lock);
+      return;
+    }
     pid_t pid = getpid();
     char buf[256];
     sprintf(buf,"/proc/%u/fd/%u",pid,dirfd); // Assume this goes well
-    char *dirpath_tmp = malloc(sizeof(char)*PATH_MAX);
 
-    if (realpath(buf,dirpath_tmp) == NULL) {
+    if (realpath(buf,dirpath) == NULL) {
       errorlog("failed resolving storage path (dirpath). path=%s\n", buf);
     }
 
-    { // Retrieve a storage id for logfile seperation
-      char *storage_id_tmp = strdup(dirpath_tmp);
-      char * p = storage_id_tmp;
-      while (*p != '\0') {
-        if (*p == '/') {
-          *p = '-';
-        }
-        p++;
+    // Retrieve a storage id for logfile seperation
+    strcpy(storage_id, dirpath);
+    char *p = storage_id;
+    while (*p != '\0') {
+      if (*p == '/') {
+        *p = '-';
       }
-      storage_id = storage_id_tmp;
+      p++;
     }
+    is_initialized = 1;
     pthread_mutex_unlock(&lock);
-    dirpath = dirpath_tmp;
 
     if (strlen(dirpath) < 8) {
       errorlog("failed configuring dirpath. Must include '/chunks/'. dirpath=%s\n", dirpath);
     }
+
+    writelog("init_once() initialized for %s\n", storage_id);
   }
 }
 
 int openat64(int dirfd, const char *pathname, int flags, mode_t mode) {
   int fd = _original_openat(dirfd, pathname, flags, mode);
+
+  init_once(dirfd);
+
+  debuglog("openat64() fd='%d', path='%s/%s'. flags='%d' mode='%lu'.\n",fd, dirpath,pathname,flags,(unsigned long)mode);
+
   // If open() failed. Return without recording it.
   if(fd == -1) {
     debuglog("openat64() org-openat64() returned -1\n");
     return fd;
   }
 
-  init_once(dirfd);
-
-  debuglog("openat64() fd='%d', path='%s/%s'. flags='%d' mode='%lu'.\n",fd, dirpath,pathname,flags,(unsigned long)mode);
-
   // return on read only operation
   if (flags == 0) {
+    open_files[fd] = SAFE_TO_IGNORE;
     return fd;
   }
 
@@ -224,41 +222,54 @@ int unlinkat(int dirfd, const char *pathname, int flags) {
 }
 
 int close(int fd) {
-  if(open_files[fd] != NULL) {
+  if (fd < 0 || fd > MAX_OPEN_FILES) {
+    errorlog("close() fd is invalid - this should never happen");
+    return _original_close(fd);
+  }
 
-    if(dirpath == NULL) {
-      errorlog("close() dirpath == NULL on close. Shouldn't happen\n");
+  const char *fd_info = open_files[fd];
+  if (fd_info == SAFE_TO_IGNORE) {
+    /* Do nothing. */
+  }
+  else if (fd_info == NULL) {
+    pid_t pid = getpid();
+    char buf[256];
+    char buf2[PATH_MAX];
+    sprintf(buf,"/proc/%u/fd/%u",pid,fd);
+    realpath(buf,buf2);
+    writelog("close()    fd='%d'. No recorded open/openat for this fd - realpath: %s\n",fd,buf2);
+  }
+  else {
+    if (!is_initialized) {
+      errorlog("close() not initialized on close. Shouldn't happen\n");
     }
 
     char filename[512];
-    snprintf(filename,512,"%s/%s",dirpath,open_files[fd]);
+    snprintf(filename,sizeof(filename),"%s/%s",dirpath,open_files[fd]);
     debuglog("close()    fd='%d', path='%s'. Writing log.\n",fd,filename);
     write_changelog("%llu m %s\n",time(NULL),filename);
     open_files[fd] = NULL;
-
-  } else {
-    writelog("close()    fd='%d', path='%s/%s'. No recorded openat for that fd, ignoring.\n",fd, dirpath,open_files[fd]);
   }
 
-  int retval = _original_close(fd);
-
-  return retval;
+  return _original_close(fd);
 }
 
 static void __attribute__((constructor)) init(void) {
-  initlog();
+  /* Make sure the log folder exists */
+  int ret = mkdir(LOGDIR, S_IRUSR | S_IWUSR | S_IXUSR);
+  if(ret != 0 && errno != EEXIST) {
+    fprintf(stderr, "Could not create logdir '%s'.\n", LOGDIR);
+    exit(1);
+  }
+
   writelog("-----------\n");
-  writelog("Initialising changelog folder...\n");
+  writelog("Library injected.\n");
 
   int retval = mkdir(CHANGELOGFOLDER, S_IRUSR | S_IWUSR | S_IXUSR);
   // If log-dir creation fails for any other reason than folder-exists, exit.
-  if(retval != 0 && errno != EEXIST) {
-    writelog("Could not create changelog folder. '%s'. Not injecting library.\n", CHANGELOGFOLDER);
+  if (retval != 0 && errno != EEXIST) {
     errorlog("Could not create changelog folder. '%s'. Not injecting library.\n", CHANGELOGFOLDER);
-    return;
-  }
-  else{
-    writelog("Library injected.\n");
+    exit(1);
   }
 
   _original_openat = (int (*)(int, const char *, int, mode_t))
@@ -267,4 +278,11 @@ static void __attribute__((constructor)) init(void) {
     dlsym(RTLD_NEXT, "unlinkat");
   _original_close = (int (*)(int))
     dlsym(RTLD_NEXT, "close");
+
+  if (_original_openat == NULL
+          || _original_unlinkat == NULL
+          || _original_close == NULL) {
+      errorlog("Cannot load original functions, we are really screwed!\n");
+      exit(1);
+  }
 }
