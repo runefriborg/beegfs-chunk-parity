a8dc125b4f4c79b6e8269824492e8f666ed0e9da
root <root@fe1.genomedk.net>
2015-11-16 13:41:27 +0100

    rechunk.sh complete

--- testing/rechunk/rechunk.sh
+++ testing/rechunk/rechunk.sh
@@ -4,14 +4,16 @@ TMPFILE=$1
 UUID=$2
 LOC=$3
 
-if [ "$LOC" == "" ]; then
-	echo "Usage rewrite.sh <tmp file - located on shared storage!> <uuid> <path to rewrite!>"
+if [ "$LOC" == "" ] || [ "$TMPFILE" == "" ] || [ "$UUID" == "" ]; then
+	echo "Usage rechunk.sh <tmp file - located on shared storage!> <uuid> <path to rechunk!>"
 	exit 1
 fi
 
 echo -n "Creating file with commands..."
-find ${LOC} -size +10M -type f \! -name '*.UUID.*' -exec echo rsync -q -a '{}' '{}.UUID.'${UUID} '&& cmp {} {}.UUID.'${UUID} '&& mv -f {}.UUID.'${UUID} '{}' '|| echo ERROR: {}' ';' > $TMPFILE
+find ${LOC} -size +10M -type f \! -name '*.UUID.*' -exec echo cp -a '"{}"' '"{}.UUID.'${UUID}'"' '&& cmp "{}" "{}.UUID.'${UUID}'"' '&& mv -f "{}.UUID.'${UUID}'"' '"{}"' '|| echo "ERROR: {}"' ';' > $TMPFILE
+
 echo "done"
 
 echo "Use this command to run using dispatch:"
+echo sbatch -N 8 --ntasks-per-node=1 --mem=1G dispatch -r ${TMPFILE} 
 
