3da80975cd2a43ad05e8f6736aaaea1e2bcb45f9
Anders Egerup Halager <aeh@birc.au.dk>
2015-11-19 10:52:10 +0100

    Specify MAX_WORKITEMS in the config file

--- src/beegfs-conf.sh
+++ src/beegfs-conf.sh
@@ -6,3 +6,11 @@ CONF_LEVELDB_INCLUDEPATH="/project/SystemWork/leveldb/include"
 
 # Settings
 CONF_BEEGFS_MOUNT="/faststorage"
+
+# MAX_ITEMS should be at least the total number of expected logical files in
+# your BeeGFS system divided by the number of storage targets plus some safety
+# margin.
+# For our system with 28 storage targets, MAX_ITEMS=25M will allow roughly
+# 700M files and use around 4GB of memory + additional memory for a hash table
+# that is dependent on the actual number of files seen.
+MAX_ITEMS=25000000ULL
